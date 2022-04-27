ebc5e40d1af9759829f9a9c11b0bb7eccb2b5bd4
Vennisha Naidoo <89158888+Vennisha-Naidoo@users.noreply.github.com>
2022-04-27 11:51:04 +0200

    Add files via upload
    This is the modified part of the Good Match

--- Program.cs
+++ Program.cs
@@ -1,10 +1,118 @@
-﻿using System;
-using System.Text.RegularExpressions;
+﻿/*******************************************
+THE COMMENTS for most of the was done done
+in part one of the question. That file
+exists in main, with this
+*******************************************/
 
-namespace GraduateAssessment
+using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace CSVFileInput
 {
     class Program
     {
+        public static String toMatch1;
+
+        static String MatchingLetters(string toMatch)
+        {
+            toMatch1 = toMatch = toMatch.Replace(" ", "");
+
+            int counter;
+            int matching;
+
+
+            String finalnumber = "";
+
+            for (int s = 0; s < toMatch1.Length; s++)
+            {
+                matching = 1;
+                counter = s + 1;
+
+                if (s == toMatch1.Length - 1)
+                {
+                    finalnumber = finalnumber + matching.ToString();
+                }
+
+                for (int i = s; counter < toMatch1.Length; counter++)
+                {
+
+                    if (counter == toMatch1.Length - 1)
+                    {
+
+                        if (toMatch1[i] == toMatch1[counter])
+                        {
+
+                            matching++;
+
+                            toMatch1 = toMatch1.Remove((counter), 1);
+
+                            finalnumber = finalnumber + matching.ToString();
+                        }
+                        else
+                        {
+
+                            finalnumber = finalnumber + matching.ToString();
+
+                        }
+
+                    }
+                    else if (toMatch1[i] == toMatch1[counter])
+                    {
+
+                        matching++;
+
+                        toMatch1 = toMatch1.Remove((counter), 1);
+
+                        counter--;
+
+                    }
+
+                }
+            }
+
+            return finalnumber;
+        }
+
+        public static string gettingpercentage(string number)
+        {
+            String percentage = "";
+
+            for (int i = 0; i < number.Length; i++)
+            {
+
+
+                double md = number.Length / 2;
+
+                double middle = Math.Ceiling(md);
+
+                if (i >= middle && number.Length % 2 == 0)
+                {
+
+
+                    break;
+
+                }
+                else if (i == middle && number.Length % 2 > 0)
+                {
+
+                    percentage = percentage + number[(int)middle].ToString();
+
+                    break;
+
+                }
+                else
+                {
+                    percentage = percentage + ((int)Char.GetNumericValue(number[i]) + (int)Char.GetNumericValue(number[number.Length - (i + 1)])).ToString();
+                }
+
+            }
+
+            return percentage;
+
+        }
+
         //Defining a new method
         static void PeopleInput()
         {
@@ -19,20 +127,12 @@ namespace GraduateAssessment
             //Taking string input, first persons name
             string PersonTwo = Console.ReadLine();
 
-            //definig an if...else statement and using REGEX to check if both inputs ONLY contains letters and not other characters
-            //The regex, explained:
-            //'^' is the start of the string
-            //'+' the character can occur one ore more times
-            //'a-z' range of small letters
-            //'A-Z' range of capital letters
-            //'$' end of the string
             if ((Regex.IsMatch(PersonOne, @"^[a-zA-Z]+$")) && (Regex.IsMatch(PersonTwo, @"^[a-zA-Z]+$")))
             {
-                //Concatanating the two string variables with 'matches', between
                 string Sentence = PersonOne + " matches " + PersonTwo;
 
 
-                String firstpart = MatchingLetters(Sentence); //Passing 'Sentence' as an argument and storing in variable, 'firstpart'
+                String firstpart = MatchingLetters(Sentence);
 
                 String part2 = gettingpercentage(firstpart);
 
@@ -69,126 +169,169 @@ namespace GraduateAssessment
 
         }
 
-        //Defining a public static string variable
-        public static String toMatch1;
 
-        //Defining a new method called, 'MatchingLetters', recieving a string parameter
-        static String MatchingLetters(string toMatch)
+        //Method is being overloadded
+        //Modified to take in two parameters, rather than user input
+        static void PeopleInput(String x, String y)
         {
-            //Replacing/removing white-spaces
-            toMatch1 = toMatch = toMatch.Replace(" ", String.Empty);
 
-            int counter; // Index of next character
-            int matching; //storing increment of matches
+            string Sentence = x + " matches " + y;
 
 
-            String finalnumber = ""; //string returned at the end
+            String firstpart = MatchingLetters(Sentence);
 
-            //nested for-loop
-            //run for the length of the string
-            for (int s = 0; s < toMatch1.Length; s++)
+            String part2 = gettingpercentage(firstpart);
+
+            do
             {
-                matching = 1; // reset counter to 1
-                counter = s + 1; //used to compare next letter in string by using '+1'
+                part2 = gettingpercentage(part2);
 
-                //If end of string, the last letter can not be matched, therefore, it's adding one to the final string
-                if (s == toMatch1.Length - 1)
-                {
-                    finalnumber = finalnumber + matching.ToString();
-                }
+            } while (part2.Length > 2);
 
-                //Looping through to compare characters
-                //i is the letter being compared to the rest of the string
-                for (int i = s; counter < toMatch1.Length; counter++)
-                {
 
-                    if (counter == toMatch1.Length - 1)
-                    {
-                        //if counter reaches the end of the string, add number of matches to the string of numbers
-                        if (toMatch1[i] == toMatch1[counter])
-                        {
+            if (Int32.Parse(part2) >= 80)
+            {
 
-                            matching++;// Increment
+                Console.WriteLine(Sentence + "\t\t" + part2 + "%, good match");
 
-                            toMatch1 = toMatch1.Remove((counter), 1);//Removing character
-
-                            finalnumber = finalnumber + matching.ToString();//Adding the count to the string of numbers
-                        }
-                        else
-                        {
-
-                            finalnumber = finalnumber + matching.ToString();
-
-                        }
-
-                    }
-                    else if (toMatch1[i] == toMatch1[counter]) //If letter 1 is matched to the next letter, in the loop
-                    {
-
-                        matching++;//Increment matched number
-
-                        toMatch1 = toMatch1.Remove((counter), 1);// remove character from the string
-
-                        counter--; //Counter decrement
-
-                    }
-
-                }
             }
-
-            return finalnumber;
-        }
-
-        //Declaring method
-        public static string gettingpercentage(string number)
-        {
-            String percentage = "";
-
-            //Declaration of for-loop
-            //Loop will run until the length of 'number' is complete
-            for (int i = 0; i < number.Length; i++)
+            else
             {
-                //find middle of 'number'
-                double md = number.Length / 2;
 
-                //Rounding up to the middle number
-                double middle = Math.Ceiling(md);
-
-                //checking if left to middle AND right to middle has a break point, if the amount of numbers are even
-                if (i >= middle && number.Length % 2 == 0)
-                {
-
-                    break;
-
-                }
-                //If the length of the number string is odd, when the iteration reaches the middle term, add the number to the overall new percentage
-                else if (i == middle && number.Length % 2 > 0)
-                {
-
-                    //Adding the odd middle number, to the overall percentage
-                    percentage = percentage + number[(int)middle].ToString();
-
-                    break;
-
-                }
-                else
-                {
-                    //adding most left to most right, and going in-ward
-                    //Passing char to int
-                    percentage = percentage + ((int)Char.GetNumericValue(number[i]) + (int)Char.GetNumericValue(number[number.Length - (i + 1)])).ToString();
-                }
+                Console.WriteLine(Sentence + "\t\t" + part2 + "%");
 
             }
 
-            return percentage;
 
         }
 
-        
+
         public static void Main(string[] args)
         {
-            //Calling the 'PeopleInput' method
-            PeopleInput();
+            //declaration of arrays - It saves each persons details
+            string[] Females = new string[5];
+            string[] Males = new string[5];
+
+            int maleCount = 0, femaleCount = 0;// Intialised to zero
+
+            //Finding file path
+            var lines = File.ReadAllLines("GraduateAssessment.csv");
+            foreach (var line in lines)// reading each line of file
+            {
+                //Last index of line
+                int lastChar = line.Length - 1;
+                //Last index finds the last character, which is the gender
+                string gen = line.Substring(lastChar);
+
+                //checking if male or female
+                if (gen == "m")
+                {
+                    //adding line to array Male[], if last character is 'm'
+                    Males[maleCount] = line;
+                    //Increment
+                    maleCount++;
+                }
+                else if (gen == "f")
+                {
+                    //adding line to array Female[], if last character is 'f'
+                    Females[femaleCount] = line;
+                    //Increment
+                    femaleCount++;
+                }
+
+
+            }
+
+            Console.WriteLine("****************************");
+
+
+            Males = Males.Distinct().ToArray();//Removes any duplicates in array
+
+            Console.WriteLine("Males: ");
+            for (int i = 0; i < Males.Length; i++)//looping through Male[] and displaying
+            {
+                Console.WriteLine(Males[i]);
+
+            }
+
+            Console.WriteLine("\n****************************");
+
+
+            Females = Females.Distinct().ToArray();//Removes any duplicates in array
+
+            Console.WriteLine("Females: ");
+            for (int i = 0; i < Females.Length; i++)//looping through Female[] and displaying
+            {
+                Console.WriteLine(Females[i]);
+
+            }
+
+            Console.WriteLine("\n****************************");
+
+            //Nested for-loop - The Good match code will run for each male matched with each female, in the CSV file
+            //Outter-loop Female
+            for (int x = 0; x < Males.Length; x++)
+            {
+                int counter = 0;//counter intialised to zero
+
+                //inner-loop is Female
+                for (int i = x; counter < Females.Length; counter++)
+                {
+                    //once condition is met, break loop
+                    if (counter == Females.Length - 2)
+                    {
+
+                        //Split getting the name
+                        string[] twoparts = Males[i].Split(',');
+
+                        //assigning the split to 'malename' variable
+                        String malename = twoparts[0];
+
+                        //Split getting the name
+                        string[] twoparts2 = Females[counter].Split(',');
+
+                        //assigning the split to 'malename' variable
+                        String femalename = twoparts2[0];
+
+                        //Passing arguments, 'malename' and 'female'
+                        PeopleInput(malename, femalename);
+
+                        break;
+
+                    }
+                    else
+                    {
+
+                        string[] twoparts = Males[i].Split(',');
+
+                        String malename = twoparts[0];
+
+                        string[] twoparts2 = Females[counter].Split(',');
+
+                        String femalename = twoparts2[0];
+
+                        PeopleInput(malename, femalename);
+
+                    }
+
+                }
+
+
+            }
+
+
+
+
+
+
+
+
+
+
+
         }
+
     }
+
 }
+
